dcc293a4375d989041fdce165318576d4a161afc
Nachiket Galande <nachiketgalande1609@gmail.com>
2025-10-27 17:16:32 +0530

    Liquid Ether Background

--- portfolio-new/src/App.tsx
+++ portfolio-new/src/App.tsx
@@ -17,6 +17,25 @@ import Certificates from "./components/Certificates";
 const App: React.FC = () => {
     return (
         <div className="App">
+            {/* <div style={{ width: "100%", height: "100vh", position: "fixed", opacity: 0.3 }}>
+                <LiquidEther
+                    colors={["#5227FF", "#FF9FFC", "#B19EEF"]}
+                    mouseForce={20}
+                    cursorSize={100}
+                    isViscous={false}
+                    viscous={30}
+                    iterationsViscous={32}
+                    iterationsPoisson={32}
+                    resolution={0.5}
+                    isBounce={false}
+                    autoDemo={true}
+                    autoSpeed={0.5}
+                    autoIntensity={2.2}
+                    takeoverDuration={0.25}
+                    autoResumeDelay={3000}
+                    autoRampDuration={0.6}
+                />
+            </div> */}
             <AnimatedBackground />
             <ScrollProgress />
             <Header />
